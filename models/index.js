997c3cfc8e59d3877439a1c9216fd4dca57c07f2
Alex Miller <ajm9594@gmail.com>
2021-07-26 13:38:19 -0700

    connected to MongoDB

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const connectionString = process.env.MONGODB_URI
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+})
+.then(() => console.log(`MongoDB Connected`))
+.catch((err) => console.log(err))
