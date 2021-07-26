1bd7f657caa781e9a96f25c78917d66996ac8161
Alex Miller <ajm9594@gmail.com>
2021-07-26 15:50:28 -0700

    find most recent report for callsign

--- controllers/reportsController.js
+++ controllers/reportsController.js
@@ -9,6 +9,27 @@ const getOneById = (req, res) => {
     })
 }
 
+const getByCall = (req, res) => {
+    console.log(`Querying ${req.params.call}`)
+    const query = db.Report.find({callLetters: req.params.call}).sort('-ts').limit(1)
+    query.exec((err, foundReport) => {
+        if (err) return console.log(err)
+        console.log(foundReport)
+        res.json(foundReport)
+    })
+}
+
+// const getStations = (req, res) => {
+//     console.log(`Querying Stations...`)
+//     db.Report.find({}, 'callLetters').exec((err, foundStations) => {
+//         if (err) return console.log(err)
+//         console.log(foundStations)
+//         res.json(foundStations)
+//     })
+// }
+
 module.exports = {
-    getOneById
+    getOneById,
+    getByCall,
+    getStations
 }
--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get('/:id', controllers.reports.getOneById)
+app.get('/station/:call', controllers.reports.getByCall)
+app.get('/stations', controllers.reports.getStations)
 app.get('/', (req, res) => {
     res.send('<h1>Sample Weather API</h1>')
 })
